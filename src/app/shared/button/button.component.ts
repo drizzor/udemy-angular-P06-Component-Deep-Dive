49bf9665f1f18ff75b607c439f54470be2c3c3cd
Kevin M <k.mary@live.be>
2024-06-27 15:53:46 +0200

    Générer label input/ztxt via component

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]',
+  selector: 'button[appButton], a[appButton]', // Attribute selector a non utilisé mais pour exemple montre que l'on peut utiliser plusieurs  attributs
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
